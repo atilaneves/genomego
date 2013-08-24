fd8bf0f1254c8462051efa1c511bab61c45a22a8
Atila Neves <aalvesne@cisco.com>
2013-08-24 17:22:21 +0200

    Tried to add individual source

--- example/ga_example.go
+++ example/ga_example.go
@@ -1,11 +1,16 @@
 package main
 
 import "fmt"
+import "ga/individual"
 
 func calcFitness() float64 {
 	return 0;
 }
 
 func main() {
-	fmt.Println("GA!");
+	fmt.Println("GA!")
+	father := individual.New(5)
+	mother := Individual.New(5)
+	children := father.crossover(mother)
+	fmt.Println("Children: %v", children)
 }
---
+++ ga/individual.go
@@ -0,0 +1,37 @@
+package individual
+
+
+type Individual struct {
+	genome []bool
+}
+
+
+func New(size uint32) *Individual {
+	ind := Individual{}
+	ind.genome = make([]bool, size)
+	return &ind
+}
+
+
+func (this *Individual) crossover(other *Individual, pos uint32) [2]*Individual {
+	if len(this.genome) == 0 {
+		panic("Empty individual!")
+	}
+	if len(other.genome) != len(this.genome) {
+		panic("Other individual has different length!");
+	}
+
+	child1 := New(uint32(len(this.genome)))
+	child2 := New(uint32(len(this.genome)))
+	for i := 0; uint32(i) < pos; i++ {
+		child1.genome[i] = this.genome[i]
+		child2.genome[i] = other.genome[i]
+	}
+
+	for i := pos; i < uint32(len(this.genome)); i++ {
+		child2.genome[i] = this.genome[i]
+		child1.genome[i] = other.genome[i]
+	}
+
+	return [...]*Individual{child1, child2}
+}
